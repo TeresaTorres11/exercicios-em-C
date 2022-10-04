d3aaa91db226add68e08a857c2bddac6ab27dccc
TeresaTorres11 <114102118+TeresaTorres11@users.noreply.github.com>
2022-10-04 13:43:12 -0300

    Add files via upload

---
+++ Projeto (Questão 4).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void main() {
+int primeiro_valor;
+int segundo_valor;
+int i, passo;
+
+printf("Por favor informe o primeiro valor ");
+scanf("%i",&primeiro_valor);
+printf("Por favor informe o segundo valor");
+scanf("%i",&segundo_valor);
+if (primeiro_valor == segundo_valor){
+   printf("valores iguais\n");
+}else {
+  if (primeiro_valor < segundo_valor) {
+    printf("O primeiro valor é menor que o segundo \n");
+    for(i=primeiro_valor;i<=segundo_valor;i++){
+      printf("%i \n",i);
+    };
+    
+  } else {
+    printf("O segundo valor é menor que o primeiro\n");
+    for(i=primeiro_valor;i>=segundo_valor;i--){
+      printf("%i \n",i);
+    };
+  };
+};
+
+}
