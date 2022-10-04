d8d0482f8d97d58d9c273e44fd259b041b57269e
TeresaTorres11 <114102118+TeresaTorres11@users.noreply.github.com>
2022-10-04 13:52:09 -0300

    Add files via upload

---
+++ Projeto (Questão 12).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int idadePedro, idadeJoana, idadeIsmael;
+
+    //Loop de validação para captação e para que as idades informadas sejam diferentes
+    do {
+        printf("Informe a idade de Pedro, Joana e Ismael: ");
+        scanf("%d %d %d", &idadePedro, &idadeJoana, &idadeIsmael);
+
+        if (idadePedro != idadeJoana && idadePedro != idadeIsmael && idadeIsmael != idadeJoana) {
+            break;
+        } else {
+            printf("Por favor, informe uma idade diferente para cada um.");
+        }
+
+    } while(1);
+
+    //Condicionais para informar quem é o mais velho
+    if (idadePedro > idadeIsmael && idadePedro > idadeJoana) {
+        printf("Pedro e o mais velho.");
+    } else if (idadeJoana > idadePedro && idadeJoana > idadeIsmael) {
+        printf("Joana e a mais velha.");
+    } else {
+        printf("Ismael e o mais velho.");
+    }
+
+    return 0;
+}
