b4efb1b7bbb12b86d220ffa64255fbdc1b6eeb69
TeresaTorres11 <114102118+TeresaTorres11@users.noreply.github.com>
2022-10-04 13:54:41 -0300

    Update Projeto (Questão 13).c

--- Projeto (Questão 13).c
+++ Projeto (Questão 13).c
@@ -8,7 +8,7 @@ int main() {
     printf("Digite um numero inteiro positivo: ");
     scanf("%u", &num);
 
-    //Condição de repetição para exibir a lista de números
+    
     printf("Lista de numeros impares menores e/ou iguais a %d: ", num);
     for(int i = 1; i <= num; i += 2) {
         printf("%d ", i);
